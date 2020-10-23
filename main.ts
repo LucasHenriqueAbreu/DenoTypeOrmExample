bf5907206fdd9944a87d197243e4d73cff52ae73
Lucas <lucasigual14@gmail.com>
2020-10-23 20:34:11 -0300

    Teste

--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
+///Teste
+
 import { App, Area } from 'https://deno.land/x/alosaur@v0.23.0/mod.ts';
 import { intTypeOrm } from "./db/create-conection.ts";
 import { UserController } from "./src/controllers/user.controller.ts";
