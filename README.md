e734db517ad0ad43da0beaa1170b308a9885ff2d
Lucas Henrique de Abreu <lucas.abreu@softfocus.com.br>
2020-10-23 13:14:26 -0300

    correçoes

---
+++ README.md
@@ -0,0 +1,4 @@
+## API Rest utilizando DENO, ALOSAUR e TYPEORM
+Sega o seguinte tutorial para criar este projeto.
+
+https://lucashenriquedeabreu.medium.com/versionamento-de-banco-de-dados-com-deno-migrations-ac7902e38574
--- main.ts
+++ main.ts
@@ -1,10 +1,8 @@
 import { App, Area } from 'https://deno.land/x/alosaur@v0.23.0/mod.ts';
+import { intTypeOrm } from "./db/create-conection.ts";
 import { UserController } from "./src/controllers/user.controller.ts";
-import { runMigrations } from "./db/run.migrations.ts";
-import { intTypeOrm } from "./db/inti-typeorm.ts";
 
 
-await runMigrations();
 await intTypeOrm();
 
 @Area({controllers: [UserController]})
